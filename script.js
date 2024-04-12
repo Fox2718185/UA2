b0706809429e2cf7adad2e7131766d75a13e3318
Fox2718185 <157029790+Fox2718185@users.noreply.github.com>
2024-04-11 22:51:51 -0400

    animation ig

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-document.getElementById('quest').onclick = function() {
-    var quest = document.getElementById('quest');
-    if (quest.style.left === '-300px') {
-        quest.style.left = '0';
-    } else {
-        quest.style.left = '-300px';
-    }
-};
-
-document.getElementById('closeButton').onclick = function() {
-    var quest = document.getElementById('quest');
-    quest.style.left = '-300px';
-};
+function hideQuest() {
+    var quest = document.querySelector('.quest');
+    quest.classList.add('clicked');
+}
+//pour cacher les instructions
