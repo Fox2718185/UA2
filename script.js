3e1dceeaf5a7ccaa1cbd9661a367f758aa52a9c5
Fox2718185 <157029790+Fox2718185@users.noreply.github.com>
2024-04-18 18:50:30 -0400

    coookies

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var catModel = document.querySelector('#cat');
+    var catModel = document.querySelector('.cat');
     catModel.addEventListener('click', function () {
         window.location.href = 'https://fox2718185.github.io/UA2/fin.html';
     });
